769e247664fd94aef3dc35b28bd97e33bb990924
Loïc PÉRON <loic.peron@gmail.com>
2019-04-21 13:37:48 +0200

    Renamed LastPass

--- Casks/lastpass.rb
+++ Casks/lastpass.rb
@@ -1,12 +1,12 @@
-cask 'lastpass-safari-extension' do
+cask 'lastpass' do
   version '4.26.0'
   sha256 '9b21c542c18824dc967f0045d685b89b3b40f2fdfe85966c178614c6640d6eda'
 
   url 'https://download.cloud.lastpass.com/mac/LastPass.dmg'
-  name 'LastPass Safari Extension'
+  name 'LastPass'
   homepage 'https://blog.lastpass.com/2019/01/change-safari-extension.html/'
 
   auto_updates true
   
-  app 'LastPass.app', target: 'LastPass Safari Extension.app'
+  app 'LastPass.app'
 end
