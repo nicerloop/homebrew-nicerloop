48442176f7d2dc28a57df6ddafb6a5cb4de24a67
Loïc PÉRON <loic.peron@gmail.com>
2019-12-14 12:24:50 +0100

    Update macOS Catalina Patcher to 1.3.0

--- Casks/macos-catalina-patcher.rb
+++ Casks/macos-catalina-patcher.rb
@@ -1,9 +1,9 @@
 cask 'macos-catalina-patcher' do
-  version '1.2.3'
-  sha256 '987944af41b0ba14fab09c9f1c02068817d55e8cfc3c1ccaf097fc9497d41830'
+  version '1.3.0'
+  sha256 '128b39b3539809a67f0cd0da03b53a61d21731d5e891c58077fc33050fb525fc'
 
   # ipfs.io/ipfs was verified as official when first introduced to the cask
-  url 'https://ipfs.io/ipfs/QmUe9qzLJmFZnLKfNJ37U7AQVtioLtohBYz5Ww2RzJayPD/macOS%20Catalina%20Patcher.dmg'
+  url 'https://ipfs.io/ipfs/Qmdw4cRrrzBWGJXNhMpBx1QfVZ3kiUbKk1eWLPnw9XxZXt/macOS%20Catalina%20Patcher.dmg'
   appcast 'http://dosdude1.com/catalina/changelog.html'
   name 'macOS Catalina Patcher'
   homepage 'http://dosdude1.com/catalina/'
