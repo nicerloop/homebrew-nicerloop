769687c0045b75344befbeed494e0ded0496008e
Loïc PÉRON <loic.peron@gmail.com>
2020-03-15 12:09:11 +0100

    Add cbz-manager

---
+++ Casks/cbz-manager.rb
@@ -0,0 +1,10 @@
+cask 'cbz-manager' do
+  version '2020-02-17'
+  sha256 '322e5cfdfe5ac255b244377d32e0a589d8a715c255ffd42a23b43c4abbe47135'
+
+  url 'https://github.com/ociviol/cbzManager/raw/master/precompiled%20binairies/Mac%20OsX/cbzManagerOsx.zip'
+  name 'cbzManager'
+  homepage 'https://ollivierciviolsoftware.wordpress.com'
+
+  app 'cbzManagerOsx.app'
+end
