4ca47fc1a68d22520a9a3cf537bd04f4d3d61330
Loïc PÉRON <loic.peron@gmail.com>
2020-04-05 08:39:04 +0200

    Update catalina-patcher to 1.4

--- Casks/macos-catalina-patcher.rb
+++ Casks/macos-catalina-patcher.rb
@@ -1,9 +1,9 @@
 cask 'macos-catalina-patcher' do
-  version '1.3.2'
-  sha256 'ebc51c6c134fb46ea8fb3c7555eadbcb7fda91c642d0ed2ede767f939cd027d8'
+  version '1.4'
+  sha256 '04d3738601591564d58ebd0ae0b99d5addf7c5a8b6529ee151b747b5883c0410'
 
   # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/dosdude1/macos-catalina-patcher/releases/download/1.3.2/macOS.Catalina.Patcher.dmg'
+  url "https://github.com/dosdude1/macos-catalina-patcher/releases/download/#{version}/macOS.Catalina.Patcher.dmg"
   appcast 'http://dosdude1.com/catalina/changelog.html'
   name 'macOS Catalina Patcher'
   homepage 'http://dosdude1.com/catalina/'
