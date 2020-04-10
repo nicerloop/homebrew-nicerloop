121345fbea43f9b67e97f13e8f7f3333ccfdba78
Loïc PÉRON <loic.peron@gmail.com>
2020-04-10 10:44:06 +0200

    Add letsview

---
+++ Casks/letsview.rb
@@ -0,0 +1,16 @@
+cask 'letsview' do
+    version '1.0.2.5'
+    sha256 '20857ad62619d035c004b0d86dec94a82060814e0358099799795e6718b3ac7c'
+  
+    # url "https://download.aoscdn.com/down.php?softid=letsview"
+    url 'https://d31gfrlvpyxnbz.cloudfront.net/letsview.pkg?2041bafef0a821237fb33913c8d0a285.pkg'
+    name 'LetsView'
+    homepage 'https://letsview.com/mac'
+  
+    pkg "letsview.pkg"
+  
+    uninstall pkgutil:   [
+                           'MacLetsView.apowersoft.com',
+                         ]
+  end
+  
