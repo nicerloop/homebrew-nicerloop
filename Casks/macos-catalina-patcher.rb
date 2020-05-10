e58161783d26764e91c4e761a733d9bd8c508787
Loïc PÉRON <loic.peron@gmail.com>
2020-05-10 16:37:22 +0200

    Update macos-catalina-patcher to version 1.4.3

--- Casks/macos-catalina-patcher.rb
+++ Casks/macos-catalina-patcher.rb
@@ -1,6 +1,6 @@
 cask 'macos-catalina-patcher' do
-  version '1.4'
-  sha256 '04d3738601591564d58ebd0ae0b99d5addf7c5a8b6529ee151b747b5883c0410'
+  version '1.4.3'
+  sha256 'b7d3de21e2f7c400093abf9000684193fa50fa770baa617b8d2291fb850ccba1'
 
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/dosdude1/macos-catalina-patcher/releases/download/#{version}/macOS.Catalina.Patcher.dmg"
