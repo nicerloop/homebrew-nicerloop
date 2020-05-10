6709270497e300ff3128184149a847e43a739a60
Loïc PÉRON <loic.peron@gmail.com>
2020-05-10 16:36:57 +0200

    Update cbzManager version to 2020-05-10

--- Casks/cbz-manager.rb
+++ Casks/cbz-manager.rb
@@ -1,6 +1,6 @@
 cask 'cbz-manager' do
-  version '2020-04-05,3dedfadeb1c979f1f3a316b613f96600375a1c69'
-  sha256 '91a3d0950e4367f84f81dedabce0e9b3db89389164efd871dbf1c3e6baf92783'
+  version '2020-05-10,5c02af6988271bb22d153bef92f57d753e0d77ba'
+  sha256 'bb513b0b20332da666f89cbb121136deb0db7339d90a4fd0362de1e25c907af9'
 
   url "https://github.com/ociviol/cbzManager/raw/#{version.after_comma}/precompiled%20binairies/Mac%20OsX/cbzManagerOsx.zip"
   name 'cbzManager'
