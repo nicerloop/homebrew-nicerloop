51c3a8b9dfcc100d750cdc15d2a5f018db482850
Loïc PÉRON <loic.peron@gmail.com>
2020-05-10 16:37:37 +0200

    Update retroactive veriosn to 1.7

--- Casks/retroactive.rb
+++ Casks/retroactive.rb
@@ -1,11 +1,11 @@
 cask 'retroactive' do
-  version '1.1'
-  sha256 '3be737d8538c363177b5a70a3b75dfb3f0b83623485607cb666fcca90a3298cb'
+  version '1.7'
+  sha256 'b77fd5e7086d958fa6dccdd9c191432f5c852230ddb6f8c36dae571bb769340c'
 
   url "https://github.com/cormiertyshawn895/Retroactive/releases/download/#{version}/Retroactive.#{version}.zip"
   appcast 'https://github.com/cormiertyshawn895/Retroactive/releases.atom'
   name 'Retroactive'
   homepage 'https://github.com/cormiertyshawn895/Retroactive'
 
-  app 'Retroactive 1.1/Retroactive (right click to open).app'
+  app "Retroactive #{version}/Retroactive (right click to open).app"
 end
