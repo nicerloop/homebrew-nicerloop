797a791f7f8f30dc9fbf914cf46ecf7a67a421c4
Loïc PÉRON <loic.peron@gmail.com>
2020-09-09 10:04:55 +0200

    Upgrade pdfbox to 2.0.21

--- Formula/pdfbox.rb
+++ Formula/pdfbox.rb
@@ -1,9 +1,9 @@
 class Pdfbox < Formula
   desc "Open source Java tool for working with PDF documents"
   homepage "https://pdfbox.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=/pdfbox/2.0.20/pdfbox-app-2.0.20.jar"
-  mirror "https://archive.apache.org/dist/pdfbox/2.0.20/pdfbox-app-2.0.20.jar"
-  sha256 "5300b92552cb6bd13a9c87ff36d1394382427e101ae2b0b02a9d9e5fd257db65"
+  url "https://www.apache.org/dyn/closer.lua?path=/pdfbox/2.0.21/pdfbox-app-2.0.21.jar"
+  mirror "https://archive.apache.org/dist/pdfbox/2.0.21/pdfbox-app-2.0.21.jar"
+  sha256 "bffefcd7e71b1907b1a2026d9e568ae102c0e1a7a998dfc305e8382530d8cc68"
 
   bottle :unneeded
 
