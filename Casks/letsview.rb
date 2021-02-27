957de806fd69cce29883fb50bda276a25d49293f
Loïc PÉRON <loic.peron@gmail.com>
2021-02-27 18:45:58 +0100

    Update letsview to use editor download url

--- Casks/letsview.rb
+++ Casks/letsview.rb
@@ -1,10 +1,10 @@
 cask "letsview" do
-  version "1.0.3.11,2041bafef0a821237fb33913c8d0a285"
+  version "1.0.3.11,b2618c54ea83a9b6f7404b47df0a053e"
   sha256 "a899ff3392d57b1386b92cbfc77cdb61184b98ca0a119f025e98b8789cf649cb"
 
-  # url "https://download.aoscdn.com/down.php?softid=letsview"
-  url "https://d31gfrlvpyxnbz.cloudfront.net/letsview.pkg?#{version.after_comma}.pkg",
-      verified: "d31gfrlvpyxnbz.cloudfront.net/letsview.pkg?#{version.after_comma}.pkg"
+  # "https://download.aoscdn.com/down.php?softid=letsview", "User-Agent: Macintosh"
+  url "https://download.apowersoft.info/letsview.pkg?#{version.after_comma}.pkg",
+      verified: "apowersoft.info/letsview"
   name "LetsView"
   desc "Turn a computer into an Airplay display"
   homepage "https://letsview.com/mac"
@@ -13,7 +13,7 @@ cask "letsview" do
     url "https://letsview.com/download-letsview"
     regex(%r{itembox mac.*<i>Version: (.*)</i>.*itembox android}i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match[0] + ",2041bafef0a821237fb33913c8d0a285" }
+      page.scan(regex).map { |match| match[0] + ",b2618c54ea83a9b6f7404b47df0a053e" }
     end
   end
 
