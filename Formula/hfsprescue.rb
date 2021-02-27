5e68a7d97b5bcfd6a889d9710cb5b7c0fabd0d14
Loïc PÉRON <loic.peron@gmail.com>
2021-02-27 18:16:55 +0100

    Update hfsprescue to 3.5

--- Formula/hfsprescue.rb
+++ Formula/hfsprescue.rb
@@ -1,9 +1,9 @@
 class Hfsprescue < Formula
   desc "Is a program to recover files from a HFS+ formatted partition"
   homepage "https://www.plop.at/en/hfsprescue/index.html"
-  url "https://download.plop.at/hfsprescue/hfsprescue-3.4-precompiled.tar.gz"
-  version "3.4"
-  sha256 "14871b71b2f3d5b8ea224d94dab1c312ec6f92c7c40e64b60bfdac44d42d4017"
+  url "https://download.plop.at/hfsprescue/hfsprescue-3.5-precompiled.tar.gz"
+  version "3.5"
+  sha256 "ea78392c6ed352944bf5e32a88ce504257a86da561b796ce49bf719f640f5c00"
 
   livecheck do
     url "https://www.plop.at/en/hfsprescue/download.html"
