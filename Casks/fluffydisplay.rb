bc624fd105c2d35272f8df667892de7b178a382a
Loïc PÉRON <loic.peron@gfi.world>
2021-04-20 16:12:15 +0200

    Add fluffydisplay

---
+++ Casks/fluffydisplay.rb
@@ -0,0 +1,11 @@
+cask "fluffydisplay" do
+  version "1.0,5"
+  sha256 "7e54120d18443a19444213ba403604c27fb0d455195f54036872cca4f92820c6"
+
+  url "https://github.com/tml1024/FluffyDisplay/releases/download/#{version.before_comma}(#{version.after_comma})/FluffyDisplay-#{version.before_comma}.#{version.after_comma}.zip"
+  name "FluffyDisplay"
+  desc "Manage virtual displays on your Mac"
+  homepage "https://github.com/tml1024/FluffyDisplay"
+
+  app "FluffyDisplay.app"
+end
