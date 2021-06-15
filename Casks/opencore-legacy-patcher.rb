8f51971bc9d7efb35614dc3115e4035ffc78f3f3
Loïc PÉRON <loic.peron@gmail.com>
2021-06-15 17:41:09 +0200

    Update opencore-legacy-patcher to 0.1.7

--- Casks/opencore-legacy-patcher.rb
+++ Casks/opencore-legacy-patcher.rb
@@ -1,6 +1,6 @@
 cask "opencore-legacy-patcher" do
-  version "0.1.6"
-  sha256 "b6d5d1b91a0305c4376ff55bb173504e734a055198fa78fc5c17d5bf9db68b31"
+  version "0.1.7"
+  sha256 "f9e8bf841af5a5d6392be639c865c628eec2776216378ee93ba45507372bdd6c"
 
   url "https://github.com/dortania/OpenCore-Legacy-Patcher/releases/download/#{version}/OpenCore-Patcher.app.zip"
   name "OpenCore Legacy Patcher"
