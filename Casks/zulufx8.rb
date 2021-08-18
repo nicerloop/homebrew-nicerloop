fe72d7da6eb355e767d6d56e33145cca88ae084a
Loïc PÉRON <loic.peron@gmail.com>
2021-08-18 10:22:06 +0200

    Align zulufx8 on zulu8

--- Casks/zulufx8.rb
+++ Casks/zulufx8.rb
@@ -1,25 +1,42 @@
 cask "zulufx8" do
-  version "8.0.302,8.56.0.21-ca-fx"
-  sha256 "904f4497c87df3e8a9611af47718cd1a743081e7a2e32f5c6bf02682d0552482"
+  if Hardware::CPU.intel?
+    version "8.0.302,8.56.0.21-ca-fx"
+    sha256 "904f4497c87df3e8a9611af47718cd1a743081e7a2e32f5c6bf02682d0552482"
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
-      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
-  name "Azul Zulu Java 8 Standard Edition Development Kit"
-  desc "Zulu build of OpenJDK - free to use without restrictions"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
 
-  livecheck do
-    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=8&os=macos&ext=dmg&bundle_type=jdk&javafx=true&release_status=ga"
-    regex(/url":"https:.*zulu(.*)-jdk(.*)-macos/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match[1] + "," + match[0] }
+    livecheck do
+      url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&arch=x86&javafx=true"
+      strategy :page_match do |page|
+        match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
+        "#{match[2]},#{match[1]}"
+      end
+    end
+  else
+    version "8.0.302,8.56.0.23-ca-fx"
+    sha256 "11fd4a652ffda4fdb614e3045b6396cde7068ec74068b6df7b5cbab6d575a4d0"
+
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+    livecheck do
+      url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&arch=arm&javafx=true"
+      strategy :page_match do |page|
+        match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
+        "#{match[2]},#{match[1]}"
+      end
     end
   end
+
+  name "Azul Zulu Java 8 Standard Edition Development Kit"
+  desc "OpenJDK distribution from Azul with JavaFX"
+  homepage "https://www.azul.com/"
 
   conflicts_with cask: "zulu8"
   depends_on macos: ">= :yosemite"
 
-  pkg "Double-Click to Install ZuluFX #{version.major}.pkg"
+  pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 end
