092d31e49e1a83dbb108b200c62c8b68acfe8ae2
Loïc PÉRON <loic.peron@gmail.com>
2021-12-08 10:37:24 +0100

    git-credential-lastpass: fix style and audit

--- Formula/git-credential-lastpass.rb
+++ Formula/git-credential-lastpass.rb
@@ -1,9 +1,8 @@
 class GitCredentialLastpass < Formula
-  desc "A credential helper for git to retrieve usernames and passwords from lastpass"
+  desc "Credential helper for git to retrieve usernames and passwords from lastpass"
   homepage "https://github.com/nicerloop/macos-scripts"
   url "https://github.com/nicerloop/macos-scripts.git", branch: "main", revision: "ac5ea502a12236875b369771b03aebc6d0c09689"
-  version "2021-12-07_2"
-
+  version "2021-12-07.2"
   depends_on "lastpass-cli"
   depends_on "pinentry-mac"
 
