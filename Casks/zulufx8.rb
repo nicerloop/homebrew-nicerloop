38744c2dbb72a7ee09da22d010a2bfcd08723f38
Loïc PÉRON <loic.peron@gmail.com>
2021-12-08 10:45:55 +0100

    zulufx8: align with zulu8 cask definition

--- Casks/zulufx8.rb
+++ Casks/zulufx8.rb
@@ -1,37 +1,30 @@
 cask "zulufx8" do
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+  choice = Hardware::CPU.intel? ? "x86" : "arm"
+
+  version "8.0.312,8.58.0.53-ca-fx"
+
   if Hardware::CPU.intel?
-    version "8.0.312,8.58.0.53-ca-fx"
     sha256 "14dbf317c29b5aa5b811f418738014a33b90cf733ce954fb5df6604a37eba495"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
-
-    livecheck do
-      url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&arch=x86&javafx=true"
-      strategy :page_match do |page|
-        match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
-        "#{match[2]},#{match[1]}"
-      end
-    end
   else
-    version "8.0.312,8.58.0.53-ca-fx"
     sha256 "1198661c812d228ff4825b4b86cc32583e34b81bb817cf9e42428d939a703ef8"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
-
-    livecheck do
-      url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&arch=arm&javafx=true"
-      strategy :page_match do |page|
-        match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
-        "#{match[2]},#{match[1]}"
-      end
-    end
   end
 
-  name "Azul Zulu Java 8 Standard Edition Development Kit"
-  desc "OpenJDK distribution from Azul with JavaFX"
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_#{arch}.dmg",
+      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  name "Azul Zulu Java 8 Standard Edition Development Kit with JavaFX"
+  desc "OpenJDK  with JavaFX distribution from Azul"
   homepage "https://www.azul.com/"
+
+  livecheck do
+    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=true&ext=dmg&os=macos&arch=#{choice}"
+    strategy :page_match do |page|
+      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.dmg/i)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
+  end
 
   conflicts_with cask: "zulu8"
   depends_on macos: ">= :yosemite"
