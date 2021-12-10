08f7ebf5a1be6468122fdf5c0b006b6f7d001d6c
Loïc PÉRON <loic.peron@gmail.com>
2021-12-10 10:20:47 +0100

    arx: add explicit version to cask definition

--- Formula/arx.rb
+++ Formula/arx.rb
@@ -2,6 +2,7 @@ class Arx < Formula
   desc "Data Anonymization Tool"
   homepage "https://arx.deidentifier.org"
   url "https://github.com/arx-deidentifier/arx/releases/download/v3.9.0/arx-3.9.0-osx-64.jar"
+  version "3.9.0"
   sha256 "d7f9b98280fe2d866dddb01c2e3f0e09c336143b5218f688645c8825f0fdb3a2"
 
   depends_on "openjdk"
