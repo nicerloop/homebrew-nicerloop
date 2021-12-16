e33c46381996296e695c655f6d1e809bd2519d60
Loïc PÉRON <loic.peron@gmail.com>
2021-12-16 23:32:32 +0100

    opencore-legacy-patcher: update to 0.3.3

--- Casks/opencore-legacy-patcher.rb
+++ Casks/opencore-legacy-patcher.rb
@@ -1,11 +1,11 @@
 cask "opencore-legacy-patcher" do
-  version "0.3.1"
-  sha256 "c6a2a4d08ca70c2e87dc102ce65248ffea0b2c974d7709df0144cdd664f2e93a"
+  version "0.3.3"
+  sha256 "4725d8715feb00403d28736f71502dcc670538808f563027a2ff043bb8dc7ca0"
 
-  url "https://github.com/dortania/OpenCore-Legacy-Patcher/releases/download/#{version}/OpenCore-Patcher-GUI.app.zip"
+  url "https://github.com/dortania/OpenCore-Legacy-Patcher/releases/download/#{version}/OpenCore-Patcher-TUI-Offline.app.zip"
   name "OpenCore Legacy Patcher"
   desc "OpenCore builder to run macOS Big Sur on unsupported macs"
   homepage "https://dortania.github.io/OpenCore-Legacy-Patcher/"
 
-  app "OpenCore Patcher.app"
+  app "OpenCore-Patcher.app"
 end
