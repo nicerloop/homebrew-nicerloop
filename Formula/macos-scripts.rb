ef618cac641f2838dba5b7bd119b9052ee391cd0
Loïc PÉRON <loic.peron@gmail.com>
2022-01-17 20:15:00 +0100

    macos-scripts: add sha256

--- Formula/macos-scripts.rb
+++ Formula/macos-scripts.rb
@@ -2,6 +2,7 @@ class MacosScripts < Formula
   desc "Various comman-line utility scripts for macOS"
   homepage "https://github.com/nicerloop/macos-scripts"
   url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "bfc47250abf9bfd10a7b3f5737a0210532450e704911aed688798c7399621ff8"
 
   def install
     bin.install "launchpad-reset"
