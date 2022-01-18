a7ae4e84da49f5df621ce0d78a6c14c2d738bcaf
Loïc PÉRON <loic.peron@gmail.com>
2022-01-18 12:18:05 +0100

    macos-scripts: update to version 1.3.0

--- Formula/macos-scripts.rb
+++ Formula/macos-scripts.rb
@@ -1,8 +1,8 @@
 class MacosScripts < Formula
   desc "Various comman-line utility scripts for macOS"
   homepage "https://github.com/nicerloop/macos-scripts"
-  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "bfc47250abf9bfd10a7b3f5737a0210532450e704911aed688798c7399621ff8"
+  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "a75a24670fc38a7ed04092779589ffff02198c3e5c3ae35bfd9267370e1ec753"
 
   def install
     bin.install "launchpad-reset"
