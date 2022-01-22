95252a0b820fe5d4a71d202ce7854588e17f7da4
Loïc PÉRON <loic.peron@gmail.com>
2022-01-22 13:03:49 +0100

    zulufx8: update to version 8.60.0.21-ca-fx

--- Casks/zulufx8.rb
+++ Casks/zulufx8.rb
@@ -2,12 +2,12 @@ cask "zulufx8" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "8.0.312,8.58.0.53-ca-fx"
+  version "8.0.322,8.60.0.21-ca-fx"
 
   if Hardware::CPU.intel?
-    sha256 "14dbf317c29b5aa5b811f418738014a33b90cf733ce954fb5df6604a37eba495"
+    sha256 "8bf213e0fc9f97fe8ff1cedea75b1490747a4f67e8a7ff160a6656b8cefed226"
   else
-    sha256 "1198661c812d228ff4825b4b86cc32583e34b81bb817cf9e42428d939a703ef8"
+    sha256 "26633a1cfa92048df8922853fb938f0893ccb25b5ead7ba8f9388d95b98ed23f"
   end
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_#{arch}.dmg",
