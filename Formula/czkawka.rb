2bea69543b25c693aea59a9ea1d58178ea61a3a4
Loïc PÉRON <loic.peron@gmail.com>
2022-01-22 13:01:08 +0100

    czkawka: update to version 4.0.0

--- Formula/czkawka.rb
+++ Formula/czkawka.rb
@@ -1,8 +1,8 @@
 class Czkawka < Formula
   desc "Multi functional app to find duplicates, empty folders, similar images etc."
   homepage "https://github.com/qarmin/czkawka"
-  url "https://github.com/qarmin/czkawka/releases/download/3.3.1/mac_czkawka_gui"
-  sha256 "5cc725879935abd26c285fbffe3ceb4a3cf11d11643924bd7f012f365d97ad0f"
+  url "https://github.com/qarmin/czkawka/releases/download/4.0.0/mac_czkawka_gui"
+  sha256 "30240352d6db05e930d51c1f5dc6b9cd5bac22481bab74f261759001f7c0fe14"
 
   depends_on "gtk+3"
   depends_on "adwaita-icon-theme"
