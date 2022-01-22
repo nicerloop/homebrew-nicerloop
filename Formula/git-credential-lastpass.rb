b64525512239330c80ae918a6f2c5f161027e94d
Loïc PÉRON <loic.peron@gmail.com>
2022-01-22 13:01:28 +0100

    git-credential-lastpass: update to version 1.3.0

--- Formula/git-credential-lastpass.rb
+++ Formula/git-credential-lastpass.rb
@@ -1,8 +1,8 @@
 class GitCredentialLastpass < Formula
   desc "Credential helper for git to retrieve usernames and passwords from lastpass"
   homepage "https://github.com/nicerloop/macos-scripts"
-  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "bfc47250abf9bfd10a7b3f5737a0210532450e704911aed688798c7399621ff8"
+  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "a75a24670fc38a7ed04092779589ffff02198c3e5c3ae35bfd9267370e1ec753"
 
   depends_on "lastpass-cli"
   depends_on "pinentry-mac"
