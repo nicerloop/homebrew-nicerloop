c973614429ea017dc34a985580e7ed196d863dec
Loïc PÉRON <loic.peron@gmail.com>
2022-02-02 13:42:32 +0100

    macos§scripts: update to v1.4.0

--- Formula/macos-scripts.rb
+++ Formula/macos-scripts.rb
@@ -1,8 +1,8 @@
 class MacosScripts < Formula
-  desc "Various comman-line utility scripts for macOS"
+  desc "Various command-line utility scripts for macOS"
   homepage "https://github.com/nicerloop/macos-scripts"
-  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "a75a24670fc38a7ed04092779589ffff02198c3e5c3ae35bfd9267370e1ec753"
+  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "54e057b77697f113b57c00e5444b016e8cd1a6d157028e80864ee90666a47d4e"
 
   def install
     bin.install "launchpad-reset"
@@ -11,6 +11,9 @@ class MacosScripts < Formula
     bin.install "rsync-backup"
     bin.install "smb-volume-mount"
     bin.install "vnc-connect"
+    bin.install "volume-icon"
     bin.install "webloc-to-url"
+    bin.install "xcode-command-line-tools-reinstall"
+    bin.install "xcode-uninstall"
   end
 end
