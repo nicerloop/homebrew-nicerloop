a20b5ba8107a43d89d7c2604c9b4427d2bcfdd04
Loïc PÉRON <loic.peron@gmail.com>
2022-02-03 17:28:08 +0100

    jailer: update to v12.1.3

--- Formula/jailer.rb
+++ Formula/jailer.rb
@@ -1,8 +1,8 @@
 class Jailer < Formula
   desc "Database Subsetting and Relational Data Browsing Tool"
   homepage "https://wisser.github.io/Jailer"
-  url "https://github.com/Wisser/Jailer/releases/download/v12.1.2/jailer_12.1.2.zip"
-  sha256 "291ff87d213e60e81dcea34c8e503e76b3e49447641c08f97f9e92f6c48d55ac"
+  url "https://github.com/Wisser/Jailer/releases/download/v12.1.3/jailer_12.1.3.zip"
+  sha256 "438a8c6c69a0907908692209d672f63d0b73a7db0176d0dbd210c6e132e9111c"
 
   depends_on "openjdk"
 
