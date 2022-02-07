36a6ba5815ca517c70c55c937503e83811d3ce10
Loïc PÉRON <loic.peron@gmail.com>
2022-02-07 17:13:06 +0100

    macos-scripts: update to v1.5.0

--- Formula/macos-scripts.rb
+++ Formula/macos-scripts.rb
@@ -1,10 +1,12 @@
 class MacosScripts < Formula
   desc "Various command-line utility scripts for macOS"
   homepage "https://github.com/nicerloop/macos-scripts"
-  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "54e057b77697f113b57c00e5444b016e8cd1a6d157028e80864ee90666a47d4e"
+  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "1e6c1dfcca8d9640ba244106fe1be2537103c68c5b280ea576d5f91d0a8037e1"
 
   def install
+    bin.install "brew-bundle-leaves"
+    bin.install "brew-bundle-patch-install-receipts"
     bin.install "launchpad-reset"
     bin.install "launchpad-sort"
     bin.install "login-picture"
