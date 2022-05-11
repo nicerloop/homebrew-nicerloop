7b62f5c8bb76728113af298cc0855ef611ff60e3
Loïc PÉRON <loic.peron@gmail.com>
2022-05-11 12:44:14 +0200

    opencore-legacy-patcher: upgrade to version 0.4.4

--- Casks/opencore-legacy-patcher.rb
+++ Casks/opencore-legacy-patcher.rb
@@ -1,8 +1,8 @@
 cask "opencore-legacy-patcher" do
-  version "0.4.3"
-  sha256 "06a7da96a0e7908c2242100fd0924f39debb4fa56edf6a884e4c3917ab20917f"
+  version "0.4.4"
+  sha256 "73939a4a623c202a76da2092ac50ab8b71e6d4d43cf0c02f84b149736db8fac7"
 
-  url "https://github.com/dortania/OpenCore-Legacy-Patcher/releases/download/#{version}/OpenCore-Patcher-GUI-Offline.app.zip"
+  url "https://github.com/dortania/OpenCore-Legacy-Patcher/releases/download/#{version}/OpenCore-Patcher-GUI.app.zip"
   name "OpenCore Legacy Patcher"
   desc "OpenCore builder to run macOS 11 (Big Sur) and 12 (Monterey) on unsupported macs"
   homepage "https://dortania.github.io/OpenCore-Legacy-Patcher/"
