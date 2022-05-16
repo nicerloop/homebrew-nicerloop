9e01c0d3ce9db3e496b86f02047977662b270290
Loïc PÉRON <loic.peron@gmail.com>
2022-05-16 22:05:18 +0200

    opencore-legacy-patcher: upgrade to version 0.4.5

--- Casks/opencore-legacy-patcher.rb
+++ Casks/opencore-legacy-patcher.rb
@@ -1,6 +1,6 @@
 cask "opencore-legacy-patcher" do
-  version "0.4.4"
-  sha256 "73939a4a623c202a76da2092ac50ab8b71e6d4d43cf0c02f84b149736db8fac7"
+  version "0.4.5"
+  sha256 "3a975a412d9e88eec1f44b096bac2cdf53ba5d9a01c39d805e021a631548b45e"
 
   url "https://github.com/dortania/OpenCore-Legacy-Patcher/releases/download/#{version}/OpenCore-Patcher-GUI.app.zip"
   name "OpenCore Legacy Patcher"
