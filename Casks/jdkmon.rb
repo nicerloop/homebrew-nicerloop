7b25251059034422050fbe594c179e00d4d08893
Loïc PÉRON <loic.peron@gmail.com>
2022-05-23 16:17:11 +0200

    jdkmon: upgrade to version 17.0.25

--- Casks/jdkmon.rb
+++ Casks/jdkmon.rb
@@ -1,13 +1,13 @@
 cask "jdkmon" do
-  version "17.0.24"
+  version "17.0.25"
 
   if Hardware::CPU.intel?
     url "https://github.com/HanSolo/JDKMon/releases/download/#{version}/JDKMon-#{version}.pkg"
-    sha256 "24f4787b32edcbbb5a9cd65d4e6d96f713adb5e35e2c70e1bdb08b756071c4c8"
+    sha256 "7295a0c082aad096659559d0cd997536ed4bbc80c2113deaccae1992b054740d"
     pkg "JDKMon-#{version}.pkg"
   else
     url "https://github.com/HanSolo/JDKMon/releases/download/#{version}/JDKMon-#{version}-aarch64.pkg"
-    sha256 "396c5b3008574352820636a9a08508e06bd0b04005a2c29ca59e3b3e08a832b9"
+    sha256 "37acb43fe9f57efa289ffe2346a3427e93143fa624530aef85ca51c517c8b152"
     pkg "JDKMon-#{version}-aarch64.pkg"
   end
 
