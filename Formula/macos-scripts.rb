fb3499f28a8e66b715c4774bb86390677db9b984
Loïc PÉRON <loic.peron@gmail.com>
2022-06-03 13:10:05 +0200

    macos-scripts: upgrade to version 1.8.0

--- Formula/macos-scripts.rb
+++ Formula/macos-scripts.rb
@@ -1,8 +1,8 @@
 class MacosScripts < Formula
   desc "Various command-line utility scripts for macOS"
   homepage "https://github.com/nicerloop/macos-scripts"
-  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "37264f45c06c087661f556a444687d606792380b4abbcba779685596ac932f80"
+  url "https://github.com/nicerloop/macos-scripts/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "5c5525764d3d71e11fda74409b1595e125ad53a58acde9624d4751af61fd7965"
 
   def install
     bin.install "brew-bundle-leaves"
@@ -10,6 +10,7 @@ class MacosScripts < Formula
     bin.install "launchpad-reset"
     bin.install "launchpad-sort"
     bin.install "login-picture"
+    bin.install "mas-versions.sh" => "mas-versions"
     bin.install "rsync-backup"
     bin.install "smb-volume-mount"
     bin.install "sudo-pam-configure"
