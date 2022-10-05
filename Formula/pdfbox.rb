efba1eab79246be6d5c2f2d6966e1709daa49b71
Loïc PÉRON <loic.peron@gmail.com>
2022-10-05 11:35:10 +0200

    pdfbox: upgrade to version 2.0.27

--- Formula/pdfbox.rb
+++ Formula/pdfbox.rb
@@ -1,9 +1,9 @@
 class Pdfbox < Formula
   desc "Open source Java tool for working with PDF documents"
   homepage "https://pdfbox.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=/pdfbox/2.0.26/pdfbox-app-2.0.26.jar"
-  mirror "https://archive.apache.org/dist/pdfbox/2.0.26/pdfbox-app-2.0.26.jar"
-  sha256 "f39cbc5e6ad354f821f559eb374a860315d699ed68c3988cd0164debd6dc7044"
+  url "https://www.apache.org/dyn/closer.lua?path=/pdfbox/2.0.27/pdfbox-app-2.0.27.jar"
+  mirror "https://archive.apache.org/dist/pdfbox/2.0.27/pdfbox-app-2.0.27.jar"
+  sha256 "2c9e46fdc92328361b21b04921f3bd26435b4c472234e283bce278f66c4c2fbc"
 
   depends_on "openjdk"
 
