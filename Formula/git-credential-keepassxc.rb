245d25716783e37b89ae60d1d1e1158d4ddaaffc
Loïc PÉRON <loic.peron@gmail.com>
2022-11-02 10:33:31 +0100

    git-credential-keepassxc: upgrade to version 0.11.0

--- Formula/git-credential-keepassxc.rb
+++ Formula/git-credential-keepassxc.rb
@@ -1,8 +1,8 @@
 class GitCredentialKeepassxc < Formula
   desc "Helper that allows Git (and shell scripts) to use KeePassXC as credential store"
   homepage "https://github.com/frederick888/git-credential-keepassxc"
-  url "https://github.com/Frederick888/git-credential-keepassxc/releases/download/v0.10.1/macos-latest-minimal.zip"
-  sha256 "4155d1b70f3c4fe49838205f38078d05be2a56117820ae7951304c7fce360972"
+  url "https://github.com/Frederick888/git-credential-keepassxc/releases/download/v0.11.0/macos-latest-minimal.zip"
+  sha256 "43a047e35df430a1bb0acf16f23f420933dfc51cc0d9fdf7dd18a5bbf58ee44e"
 
   def install
     bin.install Dir["*"]
