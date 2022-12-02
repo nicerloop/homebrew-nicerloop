fafbd92216c60ee0b439151bac4ba5725d91af00
Loïc PÉRON <loic.peron@gmail.com>
2022-12-02 23:28:51 +0100

    letsview: remove livecheck, simplified url and version

--- Casks/letsview.rb
+++ Casks/letsview.rb
@@ -1,21 +1,12 @@
 cask "letsview" do
-  version "1.1.0,b2618c54ea83a9b6f7404b47df0a053e"
+  version "1.1.0"
   sha256 "00d361fc438059d171ea6c00a271ada3f2eaac5a22153a9e61cce53f25c96599"
 
-  # "https://download.aoscdn.com/down.php?softid=letsview", "User-Agent: Macintosh"
-  url "https://download.apowersoft.info/letsview.pkg?#{version.csv.second}.pkg",
+  url "https://download.aoscdn.com/down.php?softid=letsview", user_agent: "Macintosh",
       verified: "apowersoft.info/letsview"
   name "LetsView"
   desc "Turn a computer into an Airplay display"
   homepage "https://letsview.com/mac"
-
-  livecheck do
-    url "https://letsview.com/download-letsview"
-    regex(%r{itembox mac.*<i>Version: (.*)</i>.*itembox android}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match[0] + ",b2618c54ea83a9b6f7404b47df0a053e" }
-    end
-  end
 
   pkg "letsview.pkg"
 
