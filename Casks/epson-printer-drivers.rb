96070b2d27df3d6e25917f15ec98e4da9bb00dd2
Loïc PÉRON <loic.peron@gmail.com>
2023-03-11 09:15:59 +0100

    fix: (audit) verified download url

--- Casks/epson-printer-drivers.rb
+++ Casks/epson-printer-drivers.rb
@@ -2,7 +2,8 @@ cask "epson-printer-drivers" do
   version "12.62"
   sha256 :no_check
 
-  url "https://download.epson-europe.com/pub/download/6515/epson651532eu.dmg"
+  url "https://download.epson-europe.com/pub/download/6515/epson651532eu.dmg",
+      verified: "download.epson-europe.com"
   name "Epson Printer Drivers"
   desc "Drivers for Epson printers"
   homepage "https://epson.com/"
