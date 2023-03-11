1f540191bdfab4e3c28446775eb97847c6220c6a
Loïc PÉRON <loic.peron@gmail.com>
2023-03-11 09:03:32 +0100

    fix style

--- Formula/improv-wifi-serial-client.rb
+++ Formula/improv-wifi-serial-client.rb
@@ -1,5 +1,5 @@
 class ImprovWifiSerialClient < Formula
-  desc "Configure an IoT device WiFi connection using improv-wifi serial protocol."
+  desc "Configure an IoT device WiFi connection using improv-wifi serial protocol"
   homepage "https://github.com/nicerloop/improv-wifi-serial-client"
   url "https://github.com/nicerloop/improv-wifi-serial-client/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "74c488df6c11154cd56e4feabaf0e887a9c109fa39d04fb1dbaddb616353099b"
