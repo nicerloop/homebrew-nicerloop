5961986b0fffa6f2369e9cb26035893f9f8622ef
Loïc PÉRON <loic.peron@gmail.com>
2023-05-16 12:21:02 +0200

    jdkmon: version 17.0.59

--- Casks/jdkmon.rb
+++ Casks/jdkmon.rb
@@ -1,14 +1,14 @@
 cask "jdkmon" do
-  version "17.0.57"
+  version "17.0.59"
 
   on_intel do
     url "https://github.com/HanSolo/JDKMon/releases/download/#{version}/JDKMon-#{version}.pkg"
-    sha256 "5a9a8a48f6cf109206ac16bedc527998d4c7ab2c9c123e987092be2a22e8e840"
+    sha256 "fa3e91c2da45adb8e7a4ddd7a86eb1524d4a6c3f21475ecbd97093cbc9d2b49d"
     pkg "JDKMon-#{version}.pkg"
   end
   on_arm do
     url "https://github.com/HanSolo/JDKMon/releases/download/#{version}/JDKMon-#{version}-aarch64.pkg"
-    sha256 "ea7d05c877fe26bfe64a7030ba447844be4e8bfc5cd9a319471b882df988d47b"
+    sha256 "9349ceb15efabc7b67c06b1d4bfb2a30c9861dbcc59a501da2f8fd01b0de9a67"
     pkg "JDKMon-#{version}-aarch64.pkg"
   end
 
