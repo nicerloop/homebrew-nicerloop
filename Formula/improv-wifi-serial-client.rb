ab1815df683955314b794c3d1d471e0d5c53693f
Loïc PÉRON <loic.peron@gmail.com>
2023-08-01 14:39:41 +0200

    improv-wifi-serial-client 0.4.1

--- Formula/improv-wifi-serial-client.rb
+++ Formula/improv-wifi-serial-client.rb
@@ -1,12 +1,14 @@
 class ImprovWifiSerialClient < Formula
   desc "Configure an IoT device WiFi connection using improv-wifi serial protocol"
   homepage "https://github.com/nicerloop/improv-wifi-serial-client"
-  url "https://github.com/nicerloop/improv-wifi-serial-client/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "108a51cfa835dc0dcace40d7af70b9b6408f937f59b357ddb0b7e17e63291757"
+  url "https://github.com/nicerloop/improv-wifi-serial-client/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "faaf18712de47e31cd77d3f3ec4048b859b89ff359193eabfc3059961d7c80a0"
 
   bottle do
-    root_url "https://github.com/nicerloop/homebrew-nicerloop/releases/download/improv-wifi-serial-client-0.4.0"
-    sha256 cellar: :any_skip_relocation, ventura: "83360be43bd850cd7e25a52e89c46d501ce4b1bed9393ec0d53943379db7d96e"
+    root_url "https://github.com/nicerloop/homebrew-nicerloop/releases/download/improv-wifi-serial-client-0.4.1"
+    sha256 cellar: :any_skip_relocation, ventura:  "4a2ea243bd14c0617bd819032713b80d99aeecdf3f6ca622bb8118868f158bc8"
+    sha256 cellar: :any_skip_relocation, monterey: "8b457b8e9a104bc64c4501832c1ab1554e9baaff6268bdb87a0103c936539ebb"
+    sha256 cellar: :any_skip_relocation, big_sur:  "2f5fa8b6af71db45d1383d21a3638651d081d4b7e56734c83f2d18eb21c3cda5"
   end
 
   depends_on "rust" => :build
