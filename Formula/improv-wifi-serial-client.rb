9cb4d84e72fc19d920c6535eecc985ac09382a8b
Loïc PÉRON <loic.peron@gmail.com>
2023-08-01 11:01:05 +0200

    improv-wifi-serial-client 0.4.0
    Closes #1.
    
    Signed-off-by: Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>

--- Formula/improv-wifi-serial-client.rb
+++ Formula/improv-wifi-serial-client.rb
@@ -1,8 +1,8 @@
 class ImprovWifiSerialClient < Formula
   desc "Configure an IoT device WiFi connection using improv-wifi serial protocol"
   homepage "https://github.com/nicerloop/improv-wifi-serial-client"
-  url "https://github.com/nicerloop/improv-wifi-serial-client/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "74c488df6c11154cd56e4feabaf0e887a9c109fa39d04fb1dbaddb616353099b"
+  url "https://github.com/nicerloop/improv-wifi-serial-client/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "108a51cfa835dc0dcace40d7af70b9b6408f937f59b357ddb0b7e17e63291757"
 
   depends_on "rust" => :build
 
