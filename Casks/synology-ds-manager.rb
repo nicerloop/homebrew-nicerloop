a6aed714a812ddd97f5898dbeff7baf8d5a104e5
Loïc PÉRON <loic.peron@gmail.com>
2024-01-28 23:08:18 +0100

    synology-ds-manager: user official free open-source 1.4.2 version

--- Casks/synology-ds-manager.rb
+++ Casks/synology-ds-manager.rb
@@ -1,12 +1,11 @@
 cask "synology-ds-manager" do
   version "1.4.2"
-  sha256 "bf851adbca4f1864cd914a52d3323f96ba3d9065cba29a39224d707736e97510"
+  sha256 "53cd78b6295e5405fb87ef27e199193392eecb55b67ae3e1facf8da1a7b20e72"
 
-  url "https://github.com/nicerloop/SynologyDSManager/releases/download/#{version}/SynologyDSManager-#{version}.zip",
-      verified: "github.com/nicerloop/SynologyDSManager/"
+  url "https://github.com/skavans/SynologyDSManager/releases/download/v#{version}/SynologyDSManager.zip"
   name "Synology DS Manager"
   desc "Synology Download Station application and Safari Extension"
-  homepage "https://web.archive.org/web/20220812025146/https://swiftapps.skavans.ru/synology-ds-manager-mac/"
+  homepage "https://github.com/skavans/SynologyDSManager"
 
   app "SynologyDSManager.app"
 
