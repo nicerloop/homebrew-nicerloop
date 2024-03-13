811575883f15885a867a0dbdbe9b34c847f1d929
Loïc PÉRON <loic.peron@gmail.com>
2024-03-13 10:44:27 +0100

    fixed licenses

--- Formula/svgcleaner.rb
+++ Formula/svgcleaner.rb
@@ -3,7 +3,7 @@ class Svgcleaner < Formula
   homepage "https://github.com/RazrFalcon/svgcleaner"
   url "https://github.com/RazrFalcon/svgcleaner/archive/refs/tags/v0.9.5.tar.gz"
   sha256 "dcf8dbc8939699e2e82141cb86688b6cd09da8cae5e18232ef14085c2366290c"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
   head "https://github.com/RazrFalcon/svgcleaner.git", branch: "master"
 
   depends_on "rust" => :build
