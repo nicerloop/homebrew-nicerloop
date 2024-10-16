e43dc383cae6baccca28244dc514dd551cf91598
Loïc PÉRON <loic.peron@gmail.com>
2024-10-16 09:57:55 +0200

    virtualbox@7.0 7.0.22

--- Casks/virtualbox@7.0.rb
+++ Casks/virtualbox@7.0.rb
@@ -1,74 +1,74 @@
 cask "virtualbox@7.0" do
-    version "7.0.20,163906"
-    sha256 "3d7b8833780cb274419a6209995f45889e96ad68fddb993fbeea0ad69722a821"
-  
-    url "https://download.virtualbox.org/virtualbox/#{version.csv.first}/VirtualBox-#{version.csv.first}-#{version.csv.second}-OSX.dmg"
-    name "Oracle VirtualBox"
-    desc "Virtualiser for x86 hardware"
-    homepage "https://www.virtualbox.org/"
-  
-    livecheck do
-      url "https://www.virtualbox.org/wiki/Download_Old_Builds_7_0"
-      strategy :page_match do |page|
-        match = page.match(/href=.*?VirtualBox[._-]v?(\d+(?:\.\d+)+)[._-](\d+)[._-]OSX.dmg/)
-        next if match.blank?
-  
-        "#{match[1]},#{match[2]}"
-      end
+  version "7.0.22,165102"
+  sha256 "c96d30ad330fb1115bd63d9ff11da4684cdb407f9a381488439e268ada23e664"
+
+  url "https://download.virtualbox.org/virtualbox/#{version.csv.first}/VirtualBox-#{version.csv.first}-#{version.csv.second}-OSX.dmg"
+  name "Oracle VirtualBox"
+  desc "Virtualiser for x86 hardware"
+  homepage "https://www.virtualbox.org/"
+
+  livecheck do
+    url "https://www.virtualbox.org/wiki/Download_Old_Builds_7_0"
+    strategy :page_match do |page|
+      match = page.match(/href=.*?VirtualBox[._-]v?(\d+(?:\.\d+)+)[._-](\d+)[._-]OSX.dmg/)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
     end
-  
-    conflicts_with cask: [
-      "virtualbox@6",
-      "virtualbox@beta",
-    ]
-    depends_on macos: ">= :catalina"
-    depends_on arch: :x86_64
-  
-    pkg "VirtualBox.pkg",
-        choices: [
-          {
-            "choiceIdentifier" => "choiceVBoxKEXTs",
-            "choiceAttribute"  => "selected",
-            "attributeSetting" => 1,
-          },
-          {
-            "choiceIdentifier" => "choiceVBox",
-            "choiceAttribute"  => "selected",
-            "attributeSetting" => 1,
-          },
-          {
-            "choiceIdentifier" => "choiceVBoxCLI",
-            "choiceAttribute"  => "selected",
-            "attributeSetting" => 1,
-          },
-          {
-            "choiceIdentifier" => "choiceOSXFuseCore",
-            "choiceAttribute"  => "selected",
-            "attributeSetting" => 0,
-          },
-        ]
-  
-    postflight do
-      # If VirtualBox is installed before `/usr/local/lib/pkgconfig` is created by Homebrew, it creates it itself
-      # with incorrect permissions that break other packages
-      # See https://github.com/Homebrew/homebrew-cask/issues/68730#issuecomment-534363026
-      set_ownership "/usr/local/lib/pkgconfig"
-    end
-  
-    uninstall script:  {
-                executable: "VirtualBox_Uninstall.tool",
-                args:       ["--unattended"],
-                sudo:       true,
-              },
-              pkgutil: "org.virtualbox.pkg.*",
-              delete:  "/usr/local/bin/vboximg-mount"
-  
-    zap trash: [
-          "/Library/Application Support/VirtualBox",
-          "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualbox*",
-          "~/Library/Preferences/org.virtualbox.app.VirtualBox*",
-          "~/Library/Saved Application State/org.virtualbox.app.VirtualBox*",
-          "~/Library/VirtualBox",
-        ],
-        rmdir: "~/VirtualBox VMs"
   end
+
+  conflicts_with cask: [
+    "virtualbox@6",
+    "virtualbox@beta",
+  ]
+  depends_on macos: ">= :catalina"
+  depends_on arch: :x86_64
+
+  pkg "VirtualBox.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "choiceVBoxKEXTs",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 1,
+        },
+        {
+          "choiceIdentifier" => "choiceVBox",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 1,
+        },
+        {
+          "choiceIdentifier" => "choiceVBoxCLI",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 1,
+        },
+        {
+          "choiceIdentifier" => "choiceOSXFuseCore",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
+
+  postflight do
+    # If VirtualBox is installed before `/usr/local/lib/pkgconfig` is created by Homebrew, it creates it itself
+    # with incorrect permissions that break other packages
+    # See https://github.com/Homebrew/homebrew-cask/issues/68730#issuecomment-534363026
+    set_ownership "/usr/local/lib/pkgconfig"
+  end
+
+  uninstall script:  {
+              executable: "VirtualBox_Uninstall.tool",
+              args:       ["--unattended"],
+              sudo:       true,
+            },
+            pkgutil: "org.virtualbox.pkg.*",
+            delete:  "/usr/local/bin/vboximg-mount"
+
+  zap trash: [
+        "/Library/Application Support/VirtualBox",
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualbox*",
+        "~/Library/Preferences/org.virtualbox.app.VirtualBox*",
+        "~/Library/Saved Application State/org.virtualbox.app.VirtualBox*",
+        "~/Library/VirtualBox",
+      ],
+      rmdir: "~/VirtualBox VMs"
+end
