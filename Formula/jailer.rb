66c48c87087f72bbd9ace188b44b2e74857dbdb0
Loïc PÉRON <loic.peron@gmail.com>
2024-10-24 16:36:22 +0200

    jailer 16.5.1

--- Formula/jailer.rb
+++ Formula/jailer.rb
@@ -1,8 +1,8 @@
 class Jailer < Formula
   desc "Database Subsetting and Relational Data Browsing Tool"
   homepage "https://wisser.github.io/Jailer"
-  url "https://github.com/Wisser/Jailer/releases/download/v16.5/jailer_16.5.zip"
-  sha256 "4e71752d5c9239e5970efb6c11a55ff0e8e6fd259e28af205ba3c86c66d381d3"
+  url "https://github.com/Wisser/Jailer/releases/download/v16.5.1/jailer_16.5.1.zip"
+  sha256 "3063ccf5fd44b3494e53a7ed70cef32b62429281f74b931c5237577c5ad2f301"
 
   depends_on "openjdk"
 
