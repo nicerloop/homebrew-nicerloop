ad19e68ef4551e1ca3436bf13b296b16be791ce5
Loïc PÉRON <loic.peron@gmail.com>
2025-01-22 09:51:58 +0100

    deskflow: style and audit fixes

--- Casks/deskflow@1.17.2.rb
+++ Casks/deskflow@1.17.2.rb
@@ -2,16 +2,19 @@ cask "deskflow@1.17.2" do
   arch arm: "arm64", intel: "x86_64"
 
   version "1.17.2"
-  sha256 arm:   "1d1d771fc5ac69c8440b88557e7a59f90070141881de82152c42e5945886a935", intel: "ccfa8373e770883031a33159d55bbfe063365bbd6a7274b05967aa82168fc5de"
+  sha256 arm:   "1d1d771fc5ac69c8440b88557e7a59f90070141881de82152c42e5945886a935",
+         intel: "ccfa8373e770883031a33159d55bbfe063365bbd6a7274b05967aa82168fc5de"
 
-  url "https://github.com/deskflow/deskflow/releases/download/v#{version}/deskflow-#{version}-macos-#{arch}.dmg",
-      verified: "github.com/deskflow/deskflow/"
+  url "https://github.com/deskflow/deskflow/releases/download/v#{version}/deskflow-#{version}-macos-#{arch}.dmg"
   name "Deskflow"
   desc "Mouse and keyboard sharing utility"
   homepage "https://github.com/deskflow/deskflow"
 
-  conflicts_with cask: "deskflow-dev"
+  livecheck do
+    skip "Pinned version"
+  end
 
+  conflicts_with cask: "deskflow-dev"
   depends_on macos: ">= :monterey"
 
   app "Deskflow.app"
@@ -24,7 +27,7 @@ cask "deskflow@1.17.2" do
   end
 
   zap trash: [
-     "~/Library/Saved Application State/Deskflow.savedState",
     "~/Library/Application Support/Deskflow",
+    "~/Library/Saved Application State/Deskflow.savedState",
   ]
 end
