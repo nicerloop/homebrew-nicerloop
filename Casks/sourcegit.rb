a21e0e53e39829d6256da361b8c12398ef5ca41b
Loïc PÉRON <loic.peron@gmail.com>
2025-09-28 06:46:32 +0200

    sourcegit: fix name

--- Casks/sourcegit.rb
+++ Casks/sourcegit.rb
@@ -6,7 +6,7 @@ cask "sourcegit" do
          intel: "2074db01e2a472cf70c32a59ade5c4f862b53fc5b60fa3033ed6cf269b64438d"
 
   url "https://github.com/sourcegit-scm/sourcegit/releases/download/v#{version}/sourcegit_#{version}.osx-#{arch}.zip"
-  name "sourcegit"
+  name "SourceGit"
   desc "Cross-platform GUI client for GIT users"
   homepage "https://github.com/sourcegit-scm/sourcegit"
 
