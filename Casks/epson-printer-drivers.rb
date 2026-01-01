b7e79fa7b5d1298ae2fa7367b1552055219da797
Loïc PÉRON <loic.peron@gmail.com>
2026-01-01 09:23:12 +0100

    epson-printer-driver: fix download url

--- Casks/epson-printer-drivers.rb
+++ Casks/epson-printer-drivers.rb
@@ -2,7 +2,7 @@ cask "epson-printer-drivers" do
   version "13.26,666991"
   sha256 "8491d226aa0c9c74f657a17e221aaad61cc3c5f43d3786316a9a1da8970e3170"
 
-  url "https://download.epson-europe.com/pub/download/#{version.csv.second[0, 5]}/epson#{version.csv.second}eu.dmg",
+  url "https://download.epson-europe.com/pub/download/#{version.csv.second[0, 4]}/epson#{version.csv.second}eu.dmg",
       verified: "download.epson-europe.com/"
   name "Epson Printer Drivers"
   desc "Drivers for Epson printers"
